68b56e8018508b204fcaf87b7f74a100ad2ba94c
Faruk <98034193+farukerdem34@users.noreply.github.com>
2024-09-01 13:45:07 +0300

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,1 +1,1 @@
-sudo dnf install hyprland hyprlock hypridle hyprpaper terminator pamixer mako rofi sway
+sudo dnf install hyprland hyprlock hypridle hyprpaper terminator pamixer mako rofi sway zsh vim -y
